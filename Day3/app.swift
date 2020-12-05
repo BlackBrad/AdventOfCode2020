343faf175fdfa2ef61a4a8089828c69668b0dc5b
Bradley Black <bradley_black@icloud.com>
2020-12-06 11:34:57 +1300

    Day3 Part2

--- Day3/app.swift
+++ Day3/app.swift
@@ -21,26 +21,44 @@ func get_input() -> [[Character]]{
     return geographicArray
 }
 
-func countTrees(dataArray: [[Character]]){
+func countTrees(dataArray: [[Character]], x_speed: Int, y_speed: Int) -> Int{
     var count = 0
     var current_x = 0
+    var skip_a_line = true
 
     for line in dataArray{
         if line == dataArray[0]{
             continue
         }
+        if y_speed > 1 && skip_a_line{
+            skip_a_line = false
+            continue
+        }
 
-        current_x += 3
+        current_x += x_speed
         if (current_x >= line.count){
             current_x = 0 + (current_x - line.count)
         }
         if line[current_x] == "#"{
             count += 1
         }
+        skip_a_line = true
     }
 
-    print("Tree count is \(count)")
+    return count
 }
 
 let dataArray = get_input()
-countTrees(dataArray: dataArray)
+// Part One
+let tree_count = countTrees(dataArray: dataArray, x_speed: 3, y_speed: 1)
+print("Part One: Tree count is: \(tree_count)")
+
+let countA = countTrees(dataArray: dataArray, x_speed: 1, y_speed: 1)
+let countB = tree_count
+let countC = countTrees(dataArray: dataArray, x_speed: 5, y_speed: 1)
+let countD = countTrees(dataArray: dataArray, x_speed: 7, y_speed: 1)
+let countE = countTrees(dataArray: dataArray, x_speed: 1, y_speed: 2)
+
+let part_two_answer = countA * countB * countC * countD * countE
+print("Part Two: Tree count is: \(part_two_answer)")
+
